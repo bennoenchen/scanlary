2bbfaf23c5d4555c977b333cc6880aa70f6ffe8c
Benno Biethan <benno.biethan@t-online.de>
2026-01-20 21:07:06 +0900

    Anpassung wie bennoenchen

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Scanlary
 
 Dies ist das Git-Repository für die Webseite www.scanlary.de
+
+&copy; 2026 Bennoenchen licensed under GPL-3.0-or-later
---
+++ js/script.js
@@ -0,0 +1,5 @@
+function toggleMenu(toggleBtn) {
+      const nav = document.getElementById('nav');
+      nav.classList.toggle('active');
+      toggleBtn.classList.toggle('open');
+    }
